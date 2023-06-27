e2b35e62f9db7bfb59919cd66dc08afb8760c8eb
Sanchit0102 <137860157+Sanchit0102@users.noreply.github.com>
2023-06-27 19:57:00 +0530

    info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
+API_ID       = "16536417"
+API_HASH     = "f6e58a549da642d7b765744a2f82c6d9"
+BOT_TOKEN    = "6193844202:AAF_cdZKX1W1pZJl9rttgq7OKtjuabd4LLE"
 SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = 
-ADMIN        = 
-CHANNEL      = "@CyNiTeBaCkUp"
+DATABASE_URI = "mongodb+srv://trumbot:trumbot@cluster0.cfkaeno.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001814561960
+ADMIN        = 921365334
+CHANNEL      = "@THE_SILENT_TEAMS"
